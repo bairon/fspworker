f81a460007861362017c5b01037f74bced3b941f
Aliaksandr.Sakalous <Aliaksandr.Sakalouski@seavus.com>
2017-01-11 18:18:55 +0300

    Draft

--- src/main/java/com/alsa/FspworkerApplication.java
+++ src/main/java/com/alsa/FspworkerApplication.java
@@ -1,11 +1,17 @@
 package com.alsa;
 
+import com.alsa.worker.Pinger;
 import com.alsa.worker.PrntscrSearch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static com.alsa.WebConstants.HOUR;
+import static com.alsa.WebConstants.MINUTE;
 
 @SpringBootApplication
 public class FspworkerApplication {
@@ -13,12 +19,28 @@ public class FspworkerApplication {
 	@Autowired
 	PrntscrSearch prntscrSearch;
 
+	@Autowired
+	Pinger pinger;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(FspworkerApplication.class, args);
 	}
 
 	@PostConstruct
 	public void init() {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				try {
+					pinger.ping();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}, MINUTE * 5, MINUTE * 10);
+
 		new Thread(prntscrSearch).start();
 	}
 }
---
+++ src/main/java/com/alsa/worker/Pinger.java
@@ -0,0 +1,42 @@
+package com.alsa.worker;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.RequestAcceptEncoding;
+import org.apache.http.client.protocol.ResponseContentEncoding;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * Created by alsa on 11.01.2017.
+ */@Component
+public class Pinger {
+    public HttpClient client;
+    public Pinger() {
+        RequestConfig requestConfig = RequestConfig.custom().
+                setConnectionRequestTimeout(3000).setConnectTimeout(3000).setSocketTimeout(3000).build();
+        client = HttpClientBuilder.create().addInterceptorLast(new RequestAcceptEncoding()).addInterceptorLast(new ResponseContentEncoding()).setDefaultRequestConfig(requestConfig).build();
+    }
+
+    public void ping() {
+        ping(1);
+        ping(2);
+        ping(3);
+        ping(4);
+     }
+
+    private void ping(int number) {
+        System.out.println("Pinging " + number);
+        HttpGet ping = new HttpGet("https://fspworker" + number + ".herokuapp.com/ping");
+        try {
+            client.execute(ping);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
