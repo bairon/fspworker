1aaa4abc1c9bdb714ece265a3da2171be1dcb121
Aliaksandr.Sakalous <Aliaksandr.Sakalouski@seavus.com>
2017-01-11 18:26:09 +0300

    Draft

--- src/main/java/com/alsa/worker/Worker.java
+++ src/main/java/com/alsa/worker/Worker.java
@@ -80,7 +80,7 @@ public class Worker {
                     }
                     long endT = System.currentTimeMillis();
                     long diff = endT - startT;
-                    long toSleep = 300 - diff;
+                    long toSleep = 500 - diff;
                     if (toSleep > 0) {
                         sleep(toSleep);
                     }
